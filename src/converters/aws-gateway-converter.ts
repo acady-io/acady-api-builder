5e2383d99be7a1ee87fc40f66a5034a5c1999a73
schab <christian.schab@wusoma.com>
2021-01-14 12:41:33 +0100

    feat: Support both lambda payload versions

--- src/converters/aws-gateway-converter.ts
+++ src/converters/aws-gateway-converter.ts
@@ -1,25 +1,55 @@
 import {AcadyApiRequest} from "../dto/acady-api-request";
 import {AcadyApiResponse} from "../dto/acady-api-response";
 import {ApiHeaders} from "../core/api-headers";
+import {URLSearchParams} from "url";
 
 export class AwsGatewayConverter {
     public static TYPE = "AWS_GATEWAY_HTTP";
 
     public static convertRequest(event: any): AcadyApiRequest {
         const request = new AcadyApiRequest();
-        request.method = event.requestContext.http.method;
-        request.pathName = '/' + event.pathParameters.proxy;
-        request.headers = new ApiHeaders();
-        request.queryParams = event.queryStringParameters;
-        Object.keys(event.headers).forEach(key => {
-            request.headers.append(key.toLowerCase(), event.headers[key]);
-        });
-        request.endpoint = `https://${event.requestContext.domainName}/${event.requestContext.stage}`;
-        request.fullUrl = request.endpoint + request.pathName;
-        if (event.rawQueryString.length > 0)
-            request.fullUrl += '?' + event.rawQueryString;
+        if (event.version === "1.0") {
+            request.method = event.httpMethod;
+            request.pathName = '/' + event.pathParameters.proxy;
+            request.headers = new ApiHeaders();
+            request.queryParams = event.queryStringParameters;
+            Object.keys(event.multiValueHeaders).forEach(key => {
+                const values: string[] = event.headers[key];
+                for (let value of values) {
+                    request.headers.append(key.toLowerCase(), value);
+                }
+            });
+            request.endpoint = `https://${event.requestContext.domainName}/${event.requestContext.stage}`;
 
-        console.log(request);
+            const urlSearchParams = new URLSearchParams();
+            Object.keys(event.multiValueQueryStringParameters).forEach(key => {
+                const values: string[] = event.multiValueQueryStringParameters[key];
+                for (let value of values) {
+                    urlSearchParams.append(key, value);
+                }
+            });
+
+            request.fullUrl = request.endpoint + request.pathName;
+            if (urlSearchParams.toString().length > 0)
+                request.fullUrl += '?' + urlSearchParams.toString();
+
+            console.log(request);
+        } else if (event.version === "2.0") {
+            request.method = event.requestContext.http.method;
+            request.pathName = '/' + event.pathParameters.proxy;
+            request.headers = new ApiHeaders();
+            request.queryParams = event.queryStringParameters;
+            Object.keys(event.headers).forEach(key => {
+                request.headers.append(key.toLowerCase(), event.headers[key]);
+            });
+            request.endpoint = `https://${event.requestContext.domainName}/${event.requestContext.stage}`;
+            request.fullUrl = request.endpoint + request.pathName;
+            if (event.rawQueryString.length > 0)
+                request.fullUrl += '?' + event.rawQueryString;
+
+            console.log(request);
+        }
+
 
         return request;
     }
