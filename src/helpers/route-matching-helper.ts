69ecdffa7e35d6808385b221d0b5dd18b600b148
Ilya Korzhavin <ilia-korzhavin-kitrum>
2022-06-17 12:20:43 +0300

    fix: CSY-1070 Allow several handlers

--- src/helpers/route-matching-helper.ts
+++ src/helpers/route-matching-helper.ts
@@ -1,48 +1,52 @@
-import {ApiRoute} from "../dto/api-route";
-import {AcadyApiRequest} from "../dto/acady-api-request";
+import { ApiRoute } from '../dto/api-route';
+import { AcadyApiRequest } from '../dto/acady-api-request';
 
 export class RouteMatchingHelper {
-
-    public static match(apiRoute: ApiRoute, apiRequest: AcadyApiRequest) {
-        if (apiRoute.method !== 'ANY') {
-            if (apiRoute.method !== apiRequest.method) {
-                return false;
-            }
-        }
-
-        const routePathParts = apiRoute.path.split('/');
-        const requestPathParts = apiRequest.pathName.split('/');
-
-        const pathParams = {};
-
-        for (let i = 1; i < routePathParts.length; i++) {
-            let routePathPart: string = routePathParts[i];
-            let requestPathPart: string = requestPathParts[i] ? decodeURIComponent(requestPathParts[i]) : null;
-
-            if (!requestPathPart) {
-                return false;
-            }
-
-            if (routePathPart.startsWith(':')) {
-                pathParams[routePathPart.substr(1)] = requestPathPart;
-            } else if (requestPathPart !== routePathPart) {
-                return false;
-            }
-        }
-
-        for (let i = 1; i < requestPathParts.length; i++) {
-            let routePathPart: string = routePathParts[i];
-            // let requestPathPart: string = decodeURIComponent(requestPathParts[i]);
-
-            if (!routePathPart) {
-                return false;
-            }
-        }
-
-
-        apiRequest.pathParams = pathParams;
-        apiRequest.routePath = apiRoute.path;
-
+  public static match(apiRoute: ApiRoute, apiRequest: AcadyApiRequest) {
+    if (apiRoute.method !== 'ANY') {
+      if (apiRoute.method !== apiRequest.method) {
+        return false;
+      }
+    } else {
+      if (apiRoute.path === '/') {
         return true;
+      }
     }
+
+    const routePathParts = apiRoute.path.split('/');
+    const requestPathParts = apiRequest.pathName.split('/');
+
+    const pathParams = {};
+
+    for (let i = 1; i < routePathParts.length; i++) {
+      let routePathPart: string = routePathParts[i];
+      let requestPathPart: string = requestPathParts[i]
+        ? decodeURIComponent(requestPathParts[i])
+        : null;
+
+      if (!requestPathPart) {
+        return false;
+      }
+
+      if (routePathPart.startsWith(':')) {
+        pathParams[routePathPart.substr(1)] = requestPathPart;
+      } else if (requestPathPart !== routePathPart) {
+        return false;
+      }
+    }
+
+    for (let i = 1; i < requestPathParts.length; i++) {
+      let routePathPart: string = routePathParts[i];
+      // let requestPathPart: string = decodeURIComponent(requestPathParts[i]);
+
+      if (!routePathPart) {
+        return false;
+      }
+    }
+
+    apiRequest.pathParams = pathParams;
+    apiRequest.routePath = apiRoute.path;
+
+    return true;
+  }
 }
